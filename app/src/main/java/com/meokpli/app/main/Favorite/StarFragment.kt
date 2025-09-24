f65578dc8c5bf7c39fe13506abb38c801c8db12b
tmdgus0245 <tmdgusdl0245@naver.com>
2025-09-24 22:22:02 +0900

    setting test

--- app/src/main/java/com/meokpli/app/main/Favorite/StarFragment.kt
+++ app/src/main/java/com/meokpli/app/main/Favorite/StarFragment.kt
@@ -14,6 +14,7 @@ import androidx.annotation.DrawableRes
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import com.kakao.sdk.common.util.Utility
 import com.kakao.vectormap.*
 import com.kakao.vectormap.camera.CameraUpdateFactory
 import com.kakao.vectormap.label.LabelOptions
